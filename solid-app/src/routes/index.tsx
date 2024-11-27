a232eca869f1d7460b44ae3219a50e1ffe94c675
vanelizondo15 <vanesa.elizondo@udem.edu>
2024-11-27 16:24:46 -0600

    Reapply " m"
    This reverts commit 181317a3d673dcef9c301c49c6e5e05628fedf53.

--- solid-app/src/routes/index.tsx
+++ solid-app/src/routes/index.tsx
@@ -1,7 +1,14 @@
+import { createSignal, onMount } from "solid-js";
 import MapComponentWithChart from "../components/MapaDashboard";
 import GraficoPerdidas from "../components/GraficoPerdidas";
 
 export default function Home() {
+  const [isClient, setIsClient] = createSignal(false);
+
+  onMount(() => {
+    setIsClient(true);
+  });
+
   return (
     <main class="p-8 bg-gray-100 min-h-screen">
       <h1 class="text-4xl text-sky-700 uppercase my-8 font-bold">Dashboard Principal Público</h1>
@@ -11,7 +18,7 @@ export default function Home() {
         <div class="bg-white p-4 rounded-lg shadow-md">
           <h2 class="text-2xl text-sky-700 uppercase my-4 font-bold">Mapa de Rutas y Cierres Viales</h2>
           <div style="height: 50%;">
-            <MapComponentWithChart />
+            {isClient() && <MapComponentWithChart />}
           </div>
         </div>
 
@@ -19,10 +26,10 @@ export default function Home() {
         <div class="bg-white p-4 rounded-lg shadow-md">
           <h2 class="text-2xl text-sky-700 uppercase my-4 font-bold">Gráfico de Barras de Pérdidas Económicas</h2>
           <div style="height: 50%;">
-            <GraficoPerdidas />
+            {isClient() && <GraficoPerdidas />}
           </div>
         </div>
       </div>
     </main>
   );
-} 
+}
